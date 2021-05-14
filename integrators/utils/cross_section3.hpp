2d85b73491fb4cfa1b1cf0af30455624ac585ebe
IwM341 <artem_tovstun@mail.ru>
2021-05-14 13:13:58 +0300

    lastMay

--- integrators/utils/cross_section3.hpp
+++ integrators/utils/cross_section3.hpp
@@ -125,7 +125,7 @@ extern inline dsigma_dk1_dcosTh_type dsigma_dk1_dcosTh(double mk,double mp,doubl
 						
 						double deltaE = 0.5*(k0-k1)*(k0+k1)/(Ek0+Ek1)
 						
-						std::cout<<deltaE<<" vs " << Ek0-Ek1 <<std::endl;
+						//std::cout<<deltaE<<" vs " << Ek0-Ek1 <<std::endl;
 						
 						double phase_density = 
 								( k1*k1*Ecm*(Ek0-Ek1))*sinTh1/(8*q_part*q_part*phase_2pi4*Ek1);
@@ -142,5 +142,102 @@ extern inline dsigma_dk1_dcosTh_type dsigma_dk1_dcosTh(double mk,double mp,doubl
 	
 }
 
+
+#define min(a,b) ( a < b ? a : b )
+#define max(a,b) ( a > b ? a : b )
+
+extern inline dsigma_dk1_dcosTh_type dsigma_dk1_dcosTh_Q(double mk,double mp,double k0,double q_min,MatrixElementType M2,
+								double Nphi ,double  NTh1){ 
+	const double Ek0 = E(mk,k0);
+	const double Ep0 = E(mp,k0);
+	
+	const double Ecm = Ek0+Ep0;
+	
+	
+	const PhaseState2 St2(vec4(Ek0,0,0,k0),vec4(Ep0,0,0,-k0));
+	
+	const double k1p = ((1-q_min/Ecm)*sqrt(k0*k0+q_min+q_min+2*q_min*mk*mk/Ecm-2*mk*Ek0) 
+							- q_min*(Ek0-q_min)/Ecm)/(1-2*q_min/Ecm);
+	
+	const double k1m = ((1-q_min/Ecm)*sqrt(k0*k0+q_min+q_min+2*q_min*mk*mk/Ecm-2*mk*Ek0) 
+							+ q_min*(Ek0-q_min)/Ecm)/(1-2*q_min/Ecm);
+
+	std::cout<<"cos_max = " << ( (Ecm-q_min)*(k0*k0-k1m*k1m)/(Ek0+E(mk,k1m)) - q_min*Ep0)/(q_min*k1m) << std::endl;
+	std::cout<<"cos_min = " << ( (Ecm-q_min)*(k0*k0-k1m*k1p)/(Ek0+E(mk,k1p)) - q_min*Ep0)/(q_min*k1p) << std::endl;
+
+	return [mk,mp,k0,M2,Ek0,Ep0,Ecm,St2,Nphi,NTh1,k1p,k1m](double k1,double cosTh){
+		
+		const double Ek1 = E(mk,k1);
+		const double cosTh1_max = ( (Ecm-q_min)*(k0*k0-k1*k1)/(Ek0+Ek1) - q_min*Ep0)/(q_min*k1);
+		
+		const double sinTh = sqrt(1-cosTh*cosTh);
+		
+		const double Theta1_max = acos(max(-1,min(1,cosTh1_max)));
+		
+		std::cout <<"k1, cos = " k1 << "\t" << cosTh1_max << std::endl;
+		
+		const double factor_p1p2 = 1/(4*k0*Ecm);
+		
+		
+		const vec3 e3(sinTh,0,cosTh);
+		const vec3 e1(cosTh,0,-sinTh);
+		const vec3 e2(0,1,0);	
+		const vec3 vk1 = k1*e3;	
+		const double deltaE = (k0-k1)*(k0+k1)/(Ek0+Ek1);
+		std::cout<<deltaE<<" vs " << Ek0-Ek1 <<std::endl;
+				
+		return integrateAB5([mp,k0,M2,Ek0,Ecm,Ek1,deltaE,St2,k1,Nphi](double Theta1){
+				
+				const double cosTh1 = cos(Theta1);
+				const double sinTh1 = sin(Theta1);
+ 
+				const double q_part = Ecm - Ek1 + k1*cosTh1;
+				const double q =  (Ecm*deltaE)/q_part;
+				
+				if(q_part <0)
+							std::cout<< Ecm << ", " << Ek1 << ", k1 =" << k1 << std::endl;
+				
+				std::cout <<"q vs q: "<< q << "\t" << Ecm*(Ek0-Ek1))/(Ecm- Ek1 + k1*cosTh1);
+				
+				const double phase_density = 
+								( k1*k1*Ecm*(Ek0-Ek1))*sinTh1/(8*q_part*q_part*phase_2pi4*Ek1);
+				
+				return integrateAB5([mp,M2,St2,Ek1,vk1,e1,e2,e3,sinTh1,cosTh1,q,factor_p1p2](double phi1){
+						
+						
+						const vec3 vq = q*(cosTh1*e3+sinTh1*sin(phi1)*e2+sinTh1*cos(phi1)*e1);
+						
+						
+						
+
+						PhaseState3 St3( vec4(Ek1,vk1),vec4(-vk1-vq,mp),vec4(q,vq));
+						
+							/*
+							#define COS(a,b) (a.vecPart()*b.vecPart()/a.vecPart().norm()/b.vecPart().norm())
+							if(phi1<2 && phi1 > 1 && Theta1>1 && Theta1 < 2 && cosTh>0.4 & cosTh <0.6 && k1 > 0.5*k0){
+								std::cout << "q = " << q << ", vq = " << vq.norm() << std::endl;
+								std::cout << "phi ="<<phi1<<",theta1 = "<< Theta1 <<", cosTh = "<<cosTh<<", k1 = "<< k1 <<std::endl;
+								std::cout << cosTh << "vs" << COS(St3.K1,St2.K0) << std::endl;
+								std::cout << cosTh1 << "vs" << COS(St3.K1,St3.Q) << std::endl;
+								std::cout << "St2 = "<<St2<<std::endl;
+								std::cout << "St3 = "<<St3<<std::endl;
+								std::cout << "Ecm = " << Ecm << std::endl;
+								std::cout << "k1^2,p1^2,q^2 = " << 
+												St3.K1.quad() << ", " <<
+												St3.P1.quad() << ", " << 
+												St3.Q.quad() << std::endl;
+								std::cout <<(St3.K1+St3.Q+St3.P1) <<std::endl;
+								
+								std::cout <<"Ecm - K'-P'-Q"  << Ecm - (St3.K1+St3.Q+St3.P1).t << std::endl;
+								std::cout << std::endl;
+							}*/
+
+						return M2(St3,St2)*phase_density*factor_p1p2;
+						
+					},0,2*M_PI,Nphi);
+			},0,M_PI,NTh1);
+		};
+	
+}
 #endif
 
--- integrators/utils/integrator.hpp
+++ integrators/utils/integrator.hpp
@@ -6,10 +6,7 @@
 #include <utility>
 
 
-const double Xh[5] = {0.5, 0.5+0.5*.53846931010568309105, 0.5-0.5*.53846931010568309105,
-						0.5+0.5*.90617984593866399282, 0.5-0.5*.90617984593866399282};
-const double Wh[5] = {.28444444444444444444, .23931433524968323402, .23931433524968323402,
-						.11846344252809454376, .11846344252809454376};
+
 						
 						
 						
@@ -30,8 +27,13 @@ extern inline double integrateAB(std::function<double(double)> f,double a,double
 	return sum;
 }
 
+const double Xh[5] = {0.5, 0.5+0.5*.53846931010568309105, 0.5-0.5*.53846931010568309105,
+					0.5+0.5*.90617984593866399282, 0.5-0.5*.90617984593866399282};
+const double Wh[5] = {.28444444444444444444, .23931433524968323402, .23931433524968323402,
+					.11846344252809454376, .11846344252809454376};
+
 extern inline double integrateAB5(std::function<double(double)> f,double a,double b,int N){
-	
+
 	double sum = 0.0;
 	double sum5;
 	double xi = 0;
@@ -45,5 +47,27 @@ extern inline double integrateAB5(std::function<double(double)> f,double a,doubl
 		sum += sum5*h;
 	}
 	return sum;
-}			
+}
+extern inline double integrateLogAB5(std::function<double(double)> f,double a,double b,double eps,int N){
+	const double L = b-a;
+	
+	const double T = pow(eps/(L+eps),1/N);
+	
+	double sum = 0.0;
+	double sum5;
+	
+	
+	double h = (b-a)/N;
+	#pragma omp parallel for reduction(+:sum) private(sum5,xi) shared(N,h)
+	for (int i = 0; i < N; i++) {
+		sum5 = 0.0;
+		const double xi = a - eps + (L+eps)*pow(T,i+1);
+		const double xi1 = a - eps + (L+eps)*pow(T,i);
+		const double h = xi1 - xi
+		for(int j = 0;j<5;j++)
+			sum5 += Wh[j]*f(xi + h*Xh[j]);
+		sum += sum5*h;
+	}
+	return sum;
+}				
 #endif
